847ee1919542daf6e7a35727b546b242c61ceb07
norimcode <edu.norim@yandex.ru>
2023-10-02 21:05:07 +0300

    animated donut

--- miet_map/main.js
+++ miet_map/main.js
@@ -2,6 +2,16 @@ import './style.css'
 
 import * as THREE from 'three';
 
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+
+const loader = new GLTFLoader();
+
+loader.load('src/model.glb', function(gltf){
+  scene.add(gltf.scene);
+}, undefined, function (error){
+  console.error(error)
+})
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
